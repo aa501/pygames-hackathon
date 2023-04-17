fed880d1d3c3c3970944307dde71b6f5b6ea00ca
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-04-18 01:32:36 +0530

    Updated

--- button.py
+++ button.py
@@ -1,8 +1,7 @@
 import pygame
 
 
-# button class
-class Button():
+class Button:
     def __init__(self, x, y, image, scale):
         width = image.get_width()
         height = image.get_height()
@@ -13,20 +12,32 @@ class Button():
 
     def draw(self, surface):
         action = False
-
-        # get mouse position
         pos = pygame.mouse.get_pos()
+        button_hover = self.rect.collidepoint(pos)
 
-        # check mouseover and clicked conditions
-        if self.rect.collidepoint(pos):
-            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+        # button border
+        border_color = (255, 255, 255)
+        border_thickness = 2
+        pygame.draw.rect(surface, border_color, self.rect, border_thickness)
+
+        # button background
+        bg_color = (200, 200, 200)
+        if button_hover:
+            bg_color = (180, 180, 180)
+        if self.clicked:
+            bg_color = (150, 150, 150)
+        pygame.draw.rect(surface, bg_color, self.rect)
+
+        # button image
+        image_offset = 10  # offset from the left and top edges of the button
+        surface.blit(self.image, (self.rect.x + image_offset, self.rect.y + image_offset))
+
+        # button click action
+        if button_hover:
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
                 action = True
                 self.clicked = True
-
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
-
-        # draw button
-        surface.blit(self.image, (self.rect.x, self.rect.y))
 
         return action
